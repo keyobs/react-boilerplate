c83a2b3e796a419356490bcc668025b0c55e7614
lion <zeebha@gmail.com>
2022-04-25 01:20:34 +0200

    initial configs

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # react-boilerplate
 React starter
+
+
+
+- react & react-dom
+- webpack 5
+- babel + esnext + react preset
+- typescript
+
+- files: js/jsx ts/tsx
+
+- vite
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App(){
+
+    console.log('oh la la')
+    
+    return (
+        <h1>hi, there's gonna be bugs</h1>
+    )
+}
+
+export default App;
